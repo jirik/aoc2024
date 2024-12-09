f3a5e052259c83a3a7b0a573c76e774f7e9f18d4
Jiri Kozel <jirik@users.noreply.github.com>
2024-12-09 13:15:00 +0100

    Day 9, part 2

--- day09.py
+++ day09.py
@@ -1,35 +1,26 @@
 import sys
 
 
-def get_r1(disk_map):
-    r1 = 0
-    disk_pos = 0
-    map_pos = 0
-    end_map_pos = len(disk_map) - 1
-    while map_pos <= end_map_pos:
-        is_file = map_pos % 2 == 0
-        if is_file:
-            file_id = map_pos // 2
-            while disk_map[map_pos] > 0:
-                r1 += disk_pos * file_id
-                disk_map[map_pos] -= 1
-                disk_pos += 1
-        else:
-            while disk_map[map_pos] > 0 and map_pos <= end_map_pos:
-                end_is_file = end_map_pos % 2 == 0
-                end_value = disk_map[end_map_pos]
-                if not end_is_file or end_value == 0:
-                    end_map_pos -= 1
-                    continue
-                end_file_id = end_map_pos // 2
-                r1 += end_file_id * disk_pos
-                disk_map[map_pos] -= 1
-                disk_map[end_map_pos] -= 1
-                disk_pos += 1
+def get_checksum(blocks, block_limit):
+    # slower than original get_r1, but less code in general
+    checksum = 0
 
-        map_pos += 1
+    for file_block in list(reversed(blocks))[::2]:
+        file_map_pos, file_disk_pos, file_len = file_block
+        file_id = file_map_pos // 2
+        while file_len > 0:
+            target_len_limit = min(file_len, block_limit)
+            free_block = next((b for b in blocks[1:file_map_pos:2] if b[2] >= target_len_limit), None)
+            if free_block is None:
+                target_disk_pos, target_len = file_disk_pos, file_len
+            else:
+                target_map_pos, target_disk_pos, target_len = free_block[0], free_block[1], min(free_block[2], file_len)
+                blocks[target_map_pos] = target_map_pos, target_disk_pos + target_len, free_block[2] - target_len
+            for p in range(target_disk_pos, target_disk_pos + target_len):
+                checksum += file_id * p
+            file_len -= target_len
 
-    return r1
+    return checksum
 
 
 def main():
@@ -38,8 +29,14 @@ def main():
             lines = tuple(ln.strip() for ln in f.readlines())
         disk_map = [int(c) for c in lines[0]]
 
-        r1 = get_r1(disk_map[:])
-        r2 = None
+        blocks = []
+        disk_pos = 0
+        for map_pos, length in enumerate(disk_map):
+            blocks.append((map_pos, disk_pos, length))
+            disk_pos += length
+
+        r1 = get_checksum(blocks[:], 1)
+        r2 = get_checksum(blocks[:], 10)
 
         print(file_path, r1, r2)
 
